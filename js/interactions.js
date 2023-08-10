35b482e84efe336d62a83c9abad74f7539e58d2f
Gabino Hernandez <gahergod@hotmail.com>
2023-08-10 15:21:52 -0600

    js,css,html--Header -Se quito un archivo debido a un conflicto se estilizo de forma que se tuviera un cambio interactivo en el toggle button -Se cambia de 3 lineas a una x cuando de presiona -Se cierra la ventana al momento de hacer click en un link

--- js/interactions.js
+++ js/interactions.js
@@ -1,3 +1,21 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open')
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+});
+
+
+
+
+
+
 if(window.innerWidth >= 880)
 {
 const track = document.getElementById("image-container");
--- js/toggle.js
+++
@@ -1,7 +0,0 @@
-//hola
-//
-const navToggle = document.querySelector('.nav-toggle');
-
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open')
-});
